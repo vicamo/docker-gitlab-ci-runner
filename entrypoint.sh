75c02ee90d1018aaa16eb2c237cdd54a642c209c
You-Sheng Yang (楊有勝) <vicamo@gmail.com>
2015-07-22 22:40:48 +0800

    run gitlab-ci-multi-runner at last
    Signed-off-by: You-Sheng Yang (楊有勝) <vicamo@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,6 +16,11 @@ touch ${GITLAB_CI_CONFIG}
 chown ${GITLAB_CI_USER}:${GITLAB_CI_USER} ${GITLAB_CI_CONFIG}
 chmod 0600 ${GITLAB_CI_CONFIG}
 
+# Further setups, e.g. runner registration.
+if [ -d "${GITLAB_CI_RUNNERS_DIR}" ]; then
+  eval run-parts ${GITLAB_CI_RUNNERS_ARGS:+--arg=\"${GITLAB_CI_RUNNERS_ARGS}\"} -- ${GITLAB_CI_RUNNERS_DIR}
+fi
+
 # Assign arguments unless already specified. Note that we can't go
 # Dockerfile CMD way here because CMD will not do variable
 # substitution.
@@ -34,14 +39,4 @@ start-stop-daemon --start \
   --exec ${GITLAB_CI_RUNNER_PATH} \
   --name ${GITLAB_CI_RUNNER_NAME} \
   --pidfile /var/run/${GITLAB_CI_RUNNER_NAME}.pid --make-pidfile \
-  --no-close --background \
-  -- ${args} >> /var/log/${GITLAB_CI_RUNNER_NAME}.log
-
-# Further setups, e.g. runner registration.
-if [ -d "${GITLAB_CI_RUNNERS_DIR}" ]; then
-  eval run-parts ${GITLAB_CI_RUNNERS_ARGS:+--arg=\"${GITLAB_CI_RUNNERS_ARGS}\"} -- ${GITLAB_CI_RUNNERS_DIR}
-fi
-
-while [ true ]; do
-  sleep 1000d;
-done
+  -- ${args}
